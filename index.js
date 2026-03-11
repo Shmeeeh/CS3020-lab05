23932d7c9ece22d02a817d026807526fb98e4b02
Shmeeeh <shmee@stagg.org>
2026-03-11 11:03:32 -0400

    Finished task 4, improved task 1 & 2

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 function pageLoad() {
-    document.getElementById('submit').onclick = changeText;
-    document.getElementById('enlarge').onclick = enlargeFont;
+    document.getElementById('submit').onclick = changeText; // task 1
+    document.getElementById('enlarge').onclick = enlargeFont; // task 2
     document.getElementById('shrink').onclick = shrinkFont;
-    document.getElementsByName('colors').forEach(colorTest);
-    document.getElementById('scholars').onmouseover = function(){displayDescription('scholars')};
+
+    document.getElementsByName('colors').forEach(colorTest); // task 3
+    document.getElementsByName('bg').forEach(colorTest);
+
+    document.getElementById('scholars').onmouseover = function(){displayDescription('scholars')}; // task 4
     document.getElementById('flood').onmouseover = function(){displayDescription('flood')};
     document.getElementById('wbtg').onmouseover = function(){displayDescription('wbtg')};
+
+    document.getElementById('changer').onkeydown = function(pressed) { // extension to task 1, adding Enter key compatibility
+        if (pressed.key === 'Enter') {
+            pressed.preventDefault();
+            changeText();
+        }
+    }
 }
 
 let text = 'I\'ve been changed!'; // Basic default value, unnecessary but was nice for testing
@@ -35,14 +45,23 @@ function colorTest(identity) {
             case 'black':
                 document.body.style.backgroundColor = 'black';
                 break;
-            case 'orange':
-                document.body.style.color = 'orange';
+            case 'brown':
+                document.body.style.backgroundColor = 'SaddleBrown';
+                break;
+            case 'orange': // Originally, this changed all other text colors, but I reread the assignment and saw it wanted only the modifiable text to change color.
+                // document.body.style.color = 'orange';
+                document.getElementById('subject').style.color = 'orange'
                 break;
             case 'whiteT':
-                document.body.style.color = 'white';
+                // document.body.style.color = 'white';
+                document.getElementById('subject').style.color = 'white'
                 break;
             case 'blackT':
-                document.body.style.color = 'black';
+                // document.body.style.color = 'black';
+                document.getElementById('subject').style.color = 'black'
+                break;
+            case 'blue':
+                document.getElementById('subject').style.color = 'blue'
                 break;
             default:
                 break;
@@ -51,7 +70,24 @@ function colorTest(identity) {
 }
 
 function displayDescription(imageName) {
-    console.log(imageName + ' yeahhh');
+    document.getElementById('scholars').style.borderColor = 'black';
+    document.getElementById('flood').style.borderColor = 'black';
+    document.getElementById('wbtg').style.borderColor = 'black';
+
+    document.getElementById(imageName).style.borderColor = 'red';
+    switch(imageName) {
+        case 'scholars':
+            document.getElementById('albumName').innerHTML = 'The Scholars - Car Seat Headrest'
+            break;
+        case 'flood':
+            document.getElementById('albumName').innerHTML = 'Flood - They Might Be Giants'
+            break;
+        case 'wbtg':
+            document.getElementById('albumName').innerHTML = 'we\'ve been the garages - the garages'
+            break;
+        default:
+            break;
+    }
 }
 
 window.onload = pageLoad; // have to instantiate methods on pageload
